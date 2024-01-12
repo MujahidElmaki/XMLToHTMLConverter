9c64b3f5feb2a360adb3afe7211da0775901e2da
MΛKI <mo_elmaki01@hotmail.com>
2024-01-12 10:23:30 -0500

    Initial Planning and High-Level Design for XML to HTML Conversion

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    // High level thought process
+
+    // Inside the XML are <CD> tags
+    // I would need to loop through each tag and process the attributes inside them
+    // This could probably be done using a dictionary
+
+    // Questions I have for now.
+    // Is there a global correspondence from XML to HTML?
+    // Or can I decide how I want to frame my HTML file
+    // How can I create dictionaries, loops, possibly memory management in C++?
+    // Which frameworks or libraries will I need for the console?
+
+    // Pseudo Code implementation
+
+    // 1. Init a blank HTML page that will have html, head and body tags
+    // 2. If the XML given is empty simply return the blank HTML
+    // 3. If not then proceed by parsing through the CD tags, grabbing the attributes inside them
+    // 4. Think of a way to map those corresponding values to HTML
+    // 5. Add the values to the initialized HTML page
+    // 6. Think about the user experience when the MVP is complete
 }
